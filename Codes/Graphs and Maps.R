acca89e8d3d700fe23ca826dfc67810a6f7b477c
Lappicy <lappicy@gmail.com>
2023-06-23 17:01:28 -0300

    Add files via upload

--- Codes/Graphs and Maps.R
+++ Codes/Graphs and Maps.R
@@ -1,62 +1,131 @@
-# Fazer gráfico das correlações por país ####
-correlacoes.anuais <-
-  function(tabela.dados = NULL,
-           nome.colunas = c("Desmatamento", "Crescimento_Urbano",
-                            "Crescimento_Mineracao", "Crescimento_Pastagem",
-                            "Crescimento_Agricultura", "Agua", "Outros"),
-           salvar = "Banana Verde/Resultados/Imagens/Correlacao_"){
+# Graph for Deforestation x Other classes growth ####
+gg.deforestation.cor <-
+  function(proxy.table,
+           type.one = "Growth",
+           type.two = "Deforestation",
+           color.one = c("purple", "grey50", "#EA9999", "darkorange"), 
+           color.two = "darkgreen",
+           save.as = "Results/Deforestation vs Growth.png",
+           title.name = "Analysis for Uaça 1 e 2",
+           different.groups = NULL){
     
-    # Nome dos paises
-    nome.paises <- unique(tabela.dados$Pais)
+    # Libraries required ####
+    require(ggplot2)
+    require(stats)
     
-    # rodar a correlação e fazer gráfico por país 
-    for(i in unique(nome.paises)){
-      
-      # Tabela apenas com esse pais em especifico
-      tabela.dados.pais <- tabela.dados[tabela.dados$Pais == i,]
-      
-      # Não precisa normalizar os indicadores porque são mesma escala (km2)
-      tabela.dados.pais <-
-        tabela.dados.pais[,which(colnames(tabela.dados.pais) %in% nome.colunas)]
-      
-      # Retirar linhas com NA
-      tabela.dados.pais <- na.omit(tabela.dados.pais)
-      
-      # Reorganizar ordem das colunas
-      tabela.dados.pais <- tabela.dados.pais[,c(1,3,4,5,2,6,7)]
-      
-      # Correlacionar eles
-      dados.cor.pais <- stats::cor(tabela.dados.pais)
-      
-      # Trocar nomes da tabela 
-      colnames(dados.cor.pais) <- rownames(dados.cor.pais) <-
-        c("Desmatamento", "Mineração", "Pastagem", "Agricultura",
-          "Infraestrutura Urbana", "Agua", "Outros")
-      
-      # Pegar só alguns dados da tabela
-      dados.cor.pais <- dados.cor.pais[1:5, 1:5]
-      
-      # Preparar para salvar o gráfico
-      png(file = paste0(salvar, i, ".png"), height = 900, width = 1000)
-      
-      # Fazer o gráfico
-      gg.cor.pais <-
-        corrplot::corrplot.mixed(dados.cor.pais,
-                                 lower = "number", upper = "color",
-                                 lower.col = c("red", "darkblue"),
-                                 tl.pos = "lt", diag = "u",
-                                 tl.col = "black",
-                                 tl.cex = 2, cl.cex = 2, number.cex = 2,
-                                 mar = c(1, 1, 1, 1))
-      
-      # Salvar o gráfico desse país
-      dev.off()
-      
-    }
     
-    # Retornar nada
+    # Function itself ####
+    # Prepare data ####
+    # Make sure the data is in a data.frame format.
+    proxy.table <- as.data.frame(proxy.table)
+    
+    # Make sure the Year column is numeric
+    proxy.table$Year <- as.numeric(proxy.table$Year)
+
+    # Which columns have "type.one" or "type.two" in their names
+    columns.type.one <-
+      colnames(proxy.table)[grep(type.one, colnames(proxy.table))]
+    
+    columns.type.one.names <- substr(x = columns.type.one,
+                                     start = nchar(type.one) + 2,
+                                     stop = nchar(columns.type.one))
+    
+    colnames(proxy.table)[colnames(proxy.table) %in% columns.type.one] <-
+      columns.type.one.names
+    
+    columns.type.one <- columns.type.one.names
+
+    columns.type.two <-
+      colnames(proxy.table)[grep(type.two, colnames(proxy.table))]
+    
+    
+    # Correlation analysis ####
+    # Only get type.one and type.two columns
+    cor.table <- proxy.table[,which(colnames(proxy.table) %in%
+                                      c(columns.type.two, columns.type.one))]
+ 
+    # Remove any blank data (NA)
+    cor.table <- na.omit(cor.table)
+      
+    # Correlate the data
+    suppressWarnings(cor.data <- stats::cor(cor.table)[,1])
+      
+    # Which class has the higher correlation
+    cor.information <-
+      round(max(abs(cor.data[2:length(cor.data)]), na.rm = TRUE), digits = 2)
+    
+    # Write a + or - sign accordingly
+    ifelse(max(abs(cor.data[2:length(cor.data)]), na.rm = TRUE) ==
+             max(cor.data[2:length(cor.data)], na.rm = TRUE),
+           cor.information <- paste0("+", cor.information),
+           cor.information <- paste0("-", cor.information))
+    
+    # Make sure it always has 5 characters  
+    if(nchar(cor.information) == 4) cor.information <- paste0(cor.information, "0")
+      
+    # Name the values
+    names(cor.information) <-
+      columns.type.one[which.max(abs(cor.data[2:length(cor.data)]))]
+      
+      
+    # Prepare table for ggplot2 ####
+    proxy.table <-
+      tidyr::pivot_longer(data = proxy.table,
+                          cols = c(columns.type.two, columns.type.one),
+                          names_to = "Classes",
+                          values_to = "NumValue") |>
+      dplyr::group_by_at(c("Year", different.groups, "Classes")) |>
+      dplyr::reframe(NumValue = sum(NumValue, na.rm = TRUE)) |>
+      as.data.frame()
+    
+    # Alfabetical order of type.one and type.two
+    order.types <- order(c(columns.type.one, columns.type.two))
+    
+    
+    # GGPLOT2 graph ####
+    gg.internal <-
+      
+      # Data being plotted
+      ggplot2::ggplot(data = proxy.table,
+                      aes(x = Year, y = NumValue, color = Classes)) +
+      
+      # Plot the deforestation only
+      ggplot2::geom_line(data = proxy.table[proxy.table$Classes %in%
+                                              columns.type.two,],
+                         color = color.two, linewidth = 1, alpha = 0.5) +
+      
+      # Plot other classes
+      ggplot2::geom_line(linewidth = 1, alpha = 0.3) +
+      ggplot2::geom_point(alpha = 0.5) +
+      
+      ggplot2::scale_color_manual(values = c(color.one, color.two)[order.types]) +
+      
+      ggplot2::labs(title = title.name,
+                    subtitle = paste0("Greatest correlation with ",
+                                      names(cor.information), " (",
+                                      cor.information, ")"),
+                    x = "Year", y = bquote("[" ~ km^2 ~"]"), color = "Class:") +
+      ggplot2::scale_y_continuous(labels = function(x) format(x, big.mark = ",",
+                                                              decimal.mark = ".",
+                                                              scientific = F)) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(legend.position = "top",
+                     legend.text = element_text(size = 12, color = "black"),
+                     legend.justification = "left",
+                     legend.box.just = "left",
+                     title = element_text(size = 12, color = "black", face = "bold"),
+                     text = element_text(size = 12, color = "black"),
+                     axis.text = element_text(size = 12, color = "black"))
+      
+    
+    # save where the user setted it to
+    ggplot2::ggsave(filename = save.as, plot = gg.internal,
+                    width = 3000, height = 1500, units = "px", dpi = 300)
+      
+    
+    # Returns (NULL) ####
     return(NULL)
-  }
+}
 
 
 
@@ -199,260 +268,6 @@ gg.crescimento <-
     return(NULL)
   }
 
-
-# Gráficos de Classes feitas anteriormente ####
-gg.desmatamento <-
-  function(tabela.interna,
-           eixo.y.interno = "Desmatamento",
-           salvar = paste0("Resultados/Imagens/Imagens Finais/",
-                           "Desmatamento Total.png"),
-           n.col.interno = 2,
-           cor.interno = "chartreuse4", #c(chartreuse4, grey50, #EA9999)
-           cor.texto = "black",
-           escala.grafico = "fixed",
-           ingles = TRUE){
-    
-    # Puxar a biblioteca ####
-    require(ggplot2)
-    
-    # Trabalhar com os dados da tabela ####
-    # Transformar tabela em data.frame
-    tabela.interna <- as.data.frame(tabela.interna)
-    
-    # Nome dos paises em ingles
-    if(ingles){
-      tabela.interna$Pais[tabela.interna$Pais == "Brasil"] <- "Brazil"
-      tabela.interna$Pais[tabela.interna$Pais == "Guiana Francesa"] <-
-        "French Guiana (a French Overseas Territory)"
-    }
-    
-    # Qual coluna da tabela é o escolhido
-    col.eixo.y.interno <- which(colnames(tabela.interna) == eixo.y.interno)
-    
-    # Nome do eixo y
-    nome.eixo.y <- unlist(strsplit(eixo.y.interno, split = "_"))
-    if(length(nome.eixo.y) > 1) nome.eixo.y <- paste(nome.eixo.y[1], nome.eixo.y[2])
-    if(ingles) nome.eixo.y <- "Deforestation"
-    
-    # gráfico ggplot2 ####
-    # Se tiver AP
-    if("AP" %in% colnames(tabela.interna)){
-      gg.interno <-
-        ggplot2::ggplot(data = tabela.interna,
-                        aes(x = tabela.interna[, "Ano"],
-                            y = tabela.interna[, col.eixo.y.interno],
-                            col = AP)) +
-        ggplot2::scale_color_manual(values = c("#E68613", "#8494FF"),
-                                    labels = c("Non protected Areas",
-                                               "Protected Areas")) +
-        ggplot2::geom_line() +
-        ggplot2::geom_point() +
-        ggplot2::facet_wrap(~Pais, ncol = n.col.interno, scales = escala.grafico) +
-        
-        # # Em 1989, Balbina: 1066.9950 | Balbina Completo: 1361.1609
-        # ggplot2::geom_segment(data = data.frame(Ano = rep(as.integer(2021), 2),
-        #                                         Pais = rep("Brasil", 2),
-        #                                         Desmatamento = c(615.3876, #321.2217
-        #                                                          1682.3826)),
-        #                       aes(x = Ano[1], y = Desmatamento[1],
-        #                           xend = Ano[2], yend = Desmatamento[2]),
-        #                       col = "red") +
-        
-        ggplot2::labs(x = "Year",
-                      y = bquote(.(nome.eixo.y) ~ " [" ~ km^2 ~"]"),
-                      color = NULL) +
-        ggplot2::scale_y_continuous(labels = function(x) format(x, big.mark = ",",
-                                                                decimal.mark = ".",
-                                                                scientific = F)) +
-        ggplot2::theme_bw() +
-        ggplot2::theme(text = element_text(size = 12, color = "black"),
-                       axis.text = element_text(size = 12, color = "black"),
-                       strip.text = element_text(size = 12, color = cor.texto),
-                       strip.background = element_rect(fill = cor.interno),
-                       legend.position = "top")
-    }
-    
-    # Se não tiver AP
-    if(!("AP" %in% colnames(tabela.interna))){
-      gg.interno <-
-        ggplot2::ggplot(data = tabela.interna,
-                        aes(x = tabela.interna[, "Ano"],
-                            y = tabela.interna[, col.eixo.y.interno])) +
-        ggplot2::geom_line(col = cor.interno) +
-        ggplot2::geom_point(col = cor.interno) +
-        ggplot2::facet_wrap(~Pais, ncol = n.col.interno, scales = escala.grafico) +
-        
-        ggplot2::labs(x = "Year",
-                      y = bquote(.(nome.eixo.y) ~ " [" ~ km^2 ~"]"),
-                      color = NULL) +
-        ggplot2::scale_y_continuous(labels = function(x) format(x, big.mark = ",",
-                                                                decimal.mark = ".",
-                                                                scientific = F)) +
-        ggplot2::theme_bw() +
-        ggplot2::theme(text = element_text(size = 12, color = "black"),
-                       axis.text = element_text(size = 12, color = "black"),
-                       strip.text = element_text(size = 12, color = "white"),
-                       strip.background = element_rect(fill = cor.interno),
-                       legend.position = "none")
-    }
-    
-    
-    # Se o nome "salvar" não for nulo, salvar o gráfico
-    if(!is.null(salvar)){
-      ggplot2::ggsave(filename = salvar,
-                      plot = gg.interno,
-                      width = 3000, height = 1500, units = "px", dpi = 300)
-    }
-    
-    # Retorna o gráfico se quiser usar ainda ####
-    return(gg.interno)
-  }
-
-
-
-# Gráficos de Classes feitas anteriormente para apenas um pais ####
-gg_classe_Pais <-
-  function(tabela_interna,
-           Tipo_um = "Crescimento_Mineracao",
-           Tipo_dois = "Desmatamento",
-           cor_um = "grey50", #EA9999
-           cor_dois = "darkgreen",
-           salvar = paste0("Mapas e Imagens/Escudo Guianas/",
-                           "CrescimentoMineracao_Suriname_USGS_MAPBIOMAS4.png"),
-           cor_texto = "black"){
-    
-    # Puxar a biblioteca
-    require(ggplot2)
-    
-    # Transformar tabela em data.frame
-    tabela_interna <- as.data.frame(tabela_interna)
-    
-    # Corrigir nomes, trocoando "_" por " "
-    nome_tipo_um <- unlist(strsplit(Tipo_um, split = "_"))
-    if(length(nome_tipo_um) > 1) nome_tipo_um <- paste(nome_tipo_um[1], nome_tipo_um[2])
-    nome_tipo_dois <- unlist(strsplit(Tipo_dois, split = "_"))
-    if(length(nome_tipo_dois) > 1) nome_tipo_dois <- paste(nome_tipo_dois[1], nome_tipo_dois[2])
-    
-    # gráfico ggplot2
-    gg_interno <-
-      ggplot2::ggplot(data = tabela_interna,
-                      aes(x = tabela_interna[, "Ano"],
-                          y = tabela_interna[, Tipo_um])) +
-      ggplot2::geom_line(aes(color = "Tipo 1"), linewidth = 1, alpha = 0.5) +
-      ggplot2::geom_point(aes(color = "Tipo 1")) +
-      ggplot2::geom_line(aes(y = tabela_interna[, Tipo_dois],
-                             color = "Tipo 2"),
-                         linewidth = 1, alpha = 0.5) +
-      ggplot2::geom_point(aes(y = tabela_interna[, Tipo_dois],
-                              color = "Tipo 2")) +
-      
-      ggplot2::scale_color_manual(values = c(cor_um, cor_dois),
-                                  labels = c(nome_tipo_um, nome_tipo_dois)) +
-      
-      ggplot2::labs(title = unique(tabela_interna$Pais),
-                    x = "Ano", y = bquote("[" ~ km^2 ~"]"), color = "Classe:") +
-      ggplot2::scale_y_continuous(labels = function(x) format(x, big.mark = ".",
-                                                              decimal.mark = ",",
-                                                              scientific = F)) +
-      ggplot2::theme_bw() +
-      ggplot2::theme(legend.position = "top",
-                     legend.text = element_text(size = 12, color = "black"),
-                     legend.justification = "left",
-                     legend.box.just = "left",
-                     title = element_text(size = 12, color = "black", face = "bold"),
-                     text = element_text(size = 12, color = "black"),
-                     axis.text = element_text(size = 12, color = "black"))
-    
-    # Se o nome "salvar" não for nulo, salvar o gráfico
-    if(!is.null(salvar)){
-      ggplot2::ggsave(filename = salvar,
-                      plot = gg_interno,
-                      width = 3000, height = 1500, units = "px", dpi = 300)
-    }
-    
-    # Retorna o gráfico se quiser usar ainda
-    return(gg_interno)
-  }
-
-
-
-# Função para criar um arquivo só de um argumento para TODOS os anos ####
-fun_metricas_arq <-
-  function(arg_level = "class",
-           pais = "Brasil"){
-    
-    # Nome da pasta (Tem que ter uma pasta pro país e por ano)
-    nome_pasta <- paste0("Métricas/", pais)
-    
-    
-    # Quais são os arquivos e os número de iterações
-    nomes_arq <-
-      list.files(nome_pasta)[!is.na(as.numeric(list.files(nome_pasta)))]
-    n_anos <- length(nomes_arq)
-    
-    
-    # Fazer o RBIND para cada arquivo, filtrando o que se quer
-    for(i in seq_len(n_anos)){
-      
-      # Ano correto da análise
-      ano_proxy <- as.numeric(nomes_arq[i])
-      
-      # Nome da pasta com ano
-      nome_proxy <- paste0(nome_pasta, "/", ano_proxy)
-      
-      # E quantidade de arquivos nesse wd
-      n_arq <- length(list.files(path = nome_proxy))
-      
-      
-      # Para cada ano, puxar todas os arquivos (de todas as áreas)
-      for(j in seq_len(n_arq)){
-        
-        # Definir qual a área de estudo em questão
-        nome_ae <-
-          substr(list.files(path = nome_proxy)[j], 20,
-                 nchar(list.files(path = nome_proxy)[j]) - 4) %>%
-          as.numeric()
-        
-        
-        # Puxar arquivo
-        arq_proxy <-
-          read.table(file = list.files(path = nome_proxy)[j],
-                     header = TRUE,
-                     sep = "\t",
-                     stringsAsFactors = TRUE,
-                     fileEncoding = "UTF-8") %>%
-          filter(level == arg_level) %>%
-          mutate(cod_uniq = nome_ae)
-        
-        
-        # Mudar o ano se estiver errado
-        arq_proxy$Ano <- ano_proxy
-        
-        
-        if(j == 1 & i == 1) arq_proxy_final <- arq_proxy
-        if(!(j == 1 & i == 1)) arq_proxy_final <- rbind(arq_proxy_final,
-                                                        arq_proxy)
-      }
-    }
-    
-    
-    # NÃO ESTÃ COM A PARTE DE SALVAR ATIVA (melhor fazer por fora) !!!!!!!!!!!!
-    # Salvar resumo do arquivo na pasta certa
-    #setwd(wd_save)
-    #write.table(arq_proxy_final,
-    #            row.names = FALSE,
-    #            fileEncoding = "UTF-8",
-    #            file = paste0("Resumo_", pais, "_", arg_level, ".txt"),
-    #            sep = "\t")
-    
-    
-    # Voltar para o diretório original
-    setwd(wd_arq)
-    
-    
-    # retornar valor do df
-    return(arq_proxy_final)
-  }
 
 # Mapa para um ano em específico e uma classe específica ####
 mapa_malha <- function(arq_geo = "Banana Verde/Dados/BananaVerde.gpkg",
