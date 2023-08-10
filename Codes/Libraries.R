84b2c94ad374e7e9fc140e514a9f849e7a0970ac
Lappicy <lappicy@gmail.com>
2023-08-10 15:00:40 -0300

    Add files via upload

--- Codes/Libraries.R
+++ Codes/Libraries.R
@@ -1,5 +1,31 @@
 # libraries used ####
+library(readxl)
 library(dplyr)
+library(tidyr)
+
+library(corrplot)
+library(landscapemetrics)
+
+library(ggplot2)
+library(scales)
+library(cowplot)
+library(ggspatial)
+
 library(sf)
 library(raster)
-library(stats)
+
+library(nngeo)
+
+library(dplyr)
+library(rgdal)
+library(geobr)  # doesn't exist anymore !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+library(ggspatial)
+
+library(tools)
+library(classInt) # create classes by Jenks
+
+
+# Generic options####
+options(scipen = 10000) # without scientific notation
+# memory.limit(size = 10000000) # doesn't exist anymore!!!!!!!!!!!!!!!!!!!!!!!!
+
